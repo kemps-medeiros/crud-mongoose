701587dd6ed36a9df7c128754dccd5e80f2d537e
Kemps Malavasi de Medeiros <kempsmm@gmail.com>
2021-01-31 21:03:31 -0300

    atualizações no dotenv

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+import {config} from 'dotenv';
+
 import express from 'express';
 import {studentRouter} from './routes/studentRouter.js';
 import mongoose from 'mongoose';
 
+config().parsed['PORT'];
+
 //utilizando o dotenv
-require('dotenv').config();
 
 
 //conectando com o banco de dados
 (async () => {
   try {
-  await mongoose.connect("mongodb+srv://"+process.env.USERDB+":"+process.env.PWDDB+"@bootcamp-fullstack.ldefx.mongodb.net/igti?retryWrites=true&w=majority" , {
+  await mongoose.connect("mongodb+srv://" + process.env.USERDB + ":" + process.env.PWDDB + "@bootcamp-fullstack.ldefx.mongodb.net/igti?retryWrites=true&w=majority" , {
    useNewUrlParser: true,
    useUnifiedTopology: true,
   });
@@ -27,4 +30,5 @@ app.use(express.json());
 app.use(studentRouter);
 
 app.listen(process.env.PORT, () => 
-console.log("API iniciada"));
+console.log(`API iniciada...Na porta ${process.env.PORT}`)
+);
